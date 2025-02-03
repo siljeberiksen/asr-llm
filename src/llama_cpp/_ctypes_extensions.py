78f72fe07c40ca36ad412b117c6b338a745f0359
Silje Birkeland Eriksen <siljeberiksen@gmail.com>
2025-02-03 14:45:40 +0100

    more debugging

--- src/llama_cpp/_ctypes_extensions.py
+++ src/llama_cpp/_ctypes_extensions.py
@@ -62,6 +62,7 @@ def load_shared_library(lib_base_name: str, base_path: pathlib.Path):
 
     # Try to load the shared library, handling potential errors
     for lib_path in lib_paths:
+        print("lib_path", lib_path)
         if lib_path.exists():
             try:
                 return ctypes.CDLL(str(lib_path), **cdll_args)  # type: ignore
