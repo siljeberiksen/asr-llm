2efcbdaef042f968bd0679424e77cf76fb82c82b
Silje Birkeland Eriksen <siljeberiksen@gmail.com>
2025-02-11 10:16:09 +0100

    Fix experiment 9

--- src/experiments/experiment_9/run_experiment_9.py
+++ src/experiments/experiment_9/run_experiment_9.py
@@ -30,7 +30,7 @@ except Exception as e:
         
         # Restart the script
     print("Restarting the script...")
-    os.execv(sys.executable, [sys.executable, "-m", "experiments.experiment_9.run_experiment"])
+    os.execv(sys.executable, [sys.executable, "-m", "experiments.experiment_9.run_experiment_9"])
 except BaseException as e:
     print("Caught a BaseException!")
     print(e)
