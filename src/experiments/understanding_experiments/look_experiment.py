532d7b4293ae316470bac0c62019b7024ac1188b
Silje Birkeland Eriksen <siljeberiksen@gmail.com>
2025-03-24 11:59:49 +0100

    look experiment

--- src/experiments/understanding_experiments/look_experiment.py
+++ src/experiments/understanding_experiments/look_experiment.py
@@ -61,7 +61,7 @@ def understanding_experiment(number):
     for wer_data in wer_data_points:
         beam_wer = wer_data["wer"]
         max_wer = max(beam_wer)
-        if(max_wer >= wer_data["wer_result"]):
+        if(max_wer >= wer_data["wer_result"] and max_wer !=min(beam_wer)):
             files.append(wer_data["audio_file"])
             wer_list.append(wer_data["wer_result"]*100)
         else:
@@ -91,4 +91,4 @@ def understanding_experiment(number):
     post_process(f'../result/wer_npsc_experiment_27.json', files_not_okey)
 
 
-understanding_experiment(39)
+understanding_experiment(35)
