6f009978f5ae5d6537389ff7293491eae04bd38b
Silje Birkeland Eriksen <siljeberiksen@gmail.com>
2025-04-22 10:57:24 +0200

    58 experiment setup

---
+++ src/experiments/understanding_experiments/filteredvs_non_filtered.py
@@ -0,0 +1,169 @@
+import json
+import statistics
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from visualization.creating_distribution_of_wer import creating_distribution
+def post_process(file_name, empty_instances = []):
+    with open(f'../result/{file_name}', 'r') as file:
+        wer_data = json.load(file)
+
+    print("Processing: ", file_name)
+    wer_best_beam = []
+    cer_best_beam = []
+    wer_worst_beam = []
+    cer_worst_beam = []
+    wer_sum = 0
+    cer_sum = 0
+    wer_acutal =[]
+    cer_actual = []
+    length = 0
+    difference_beams = []
+    differnece_beams_cer =[]
+    for data in wer_data:
+        if(data["audio_file"] in empty_instances):
+            continue
+        cer_best_beam.append(min(data["cer"]))
+        wer_worst_beam.append(max(data["wer"]))
+        cer_worst_beam.append(max(data["cer"]))
+        difference_beams.append(max(data["wer"])-min(data["wer"]))
+        differnece_beams_cer.append(max(data["cer"])-min(data["cer"]))
+
+        wer_acutal.append(data["wer_result"])
+        cer_actual.append(data["cer_result"])
+
+        wer_sum += data["wer_result"]
+        cer_sum += data["cer_result"]
+        length +=1
+    print("Average wer beam", sum(difference_beams)/length)
+    #print(difference_beams)
+
+    print(length)
+
+    print("Count", difference_beams.count(0))
+
+    print("Average cer bemas", sum(differnece_beams_cer)/length)
+    print("Count", differnece_beams_cer.count(0))
+    median_value = statistics.median(wer_acutal)
+    print("median_wer", median_value)
+
+    median_value= statistics.median(cer_actual)
+    print("median_cer", median_value)
+
+    print("average wer", wer_sum/length)
+    print("Standard deviation wer", statistics.stdev(wer_acutal))
+    print("average cer", cer_sum/length)
+    print("Standard devision cer", statistics.stdev(cer_actual))
+    return  wer_sum/length, wer_acutal
+
+def understanding_experiment(number):
+    with open(f'../result/wer_npsc_experiment_{number}_llm.json', 'r') as file:
+        wer_data_points = json.load(file)
+    files = []
+    files_not_okey = []
+    wer_list = []
+    for wer_data in wer_data_points:
+        beam_wer = wer_data["wer"]
+        max_wer = max(beam_wer)
+        if(max_wer >= wer_data["wer_result"] and max_wer !=min(beam_wer)):
+            files.append(wer_data["audio_file"])
+            wer_list.append(wer_data["wer_result"]*100)
+        else:
+            files_not_okey.append(wer_data["audio_file"])
+    print("Results better", len(files_not_okey))
+    print("Results total", len(wer_data_points))
+    data_points_to_add =[]
+
+    #creating_distribution(wer_list)
+
+    with open(f'../result/beam_npsc_experiment_{number}_llm.json', 'r') as file:
+        beam_data_points = json.load(file)
+        for beam_data in beam_data_points:
+            if(beam_data["audio_file"] in files_not_okey):
+                data_point_to_add = {
+                    "beams": beam_data["beams"] ,
+                    "transcribed": beam_data["transcribed"],
+                    "audio_file": beam_data["audio_file"],
+                }
+                data_points_to_add.append(data_point_to_add)
+        print(len(data_points_to_add))
+
+    with open(f'../result/filtering_empty_experiment_{number}_llm.json', "w+") as file:
+        file.write(json.dumps(data_points_to_add, indent=4))
+    mean_filter, wer_values = post_process(f'../result/wer_npsc_experiment_{number}_llm.json', files_not_okey)
+    mean_non_filter, wer_values_filter = post_process(f'../result/wer_npsc_experiment_{number}_llm.json')
+    print("Basline")
+    mean_filter_basleine, wer_values_filter_baseline = post_process(f'../result/wer_npsc_experiment_27.json', files_not_okey)
+    return mean_filter, wer_values, mean_non_filter, wer_values_filter, mean_filter_basleine, wer_values_filter_baseline
+
+# History 10
+mean_filter_10, wer_values_llm_full_10, mean_non_filter_10, wer_values_llm_filtered_10, mean_filter_baseline_10, wer_values_baseline_filtered_10 = understanding_experiment(7)
+
+# History 50
+mean_filter_50, wer_values_llm_full_50, mean_non_filter_50, wer_values_llm_filtered_50, mean_filter_baseline_50, wer_values_baseline_filtered_50 = understanding_experiment(6)
+
+# History 100
+mean_filter_100, wer_values_llm_full_100, mean_non_filter_100, wer_values_llm_filtered_100, mean_filter_baseline_100, wer_values_baseline_filtered_100 = understanding_experiment(5)
+
+
+# History 10
+mean_filter_10_llama, wer_values_llm_full_10_llama, mean_non_filter_10, wer_values_llm_filtered_10, mean_filter_baseline_10_llama, wer_values_baseline_filtered_10 = understanding_experiment(9)
+
+# History 50
+mean_filter_50_llama, wer_values_llm_full_50_llama, mean_non_filter_50, wer_values_llm_filtered_50, mean_filter_baseline_50_llama, wer_values_baseline_filtered_50 = understanding_experiment(10)
+
+# History 100
+mean_filter_100_llama, wer_values_llm_full_100_llama, mean_non_filter_100, wer_values_llm_filtered_100, mean_filter_baseline_100_llama, wer_values_baseline_filtered_100 = understanding_experiment(11)
+
+mean_baseline, wer_values_baseline = post_process(f'../result/wer_npsc_experiment_27.json')
+
+mean_filter_10
+mean_filter_50
+mean_filter_100
+
+mean_df = pd.DataFrame({
+    'History': [10, 10, 50, 50, 100, 100],
+    'Filtering based on LLM': ['Baseline filtering Gemma', 'Baseline filtering LLaMA'] * 3,
+    'Mean_WER': [
+        mean_filter_baseline_10*100, mean_filter_baseline_10_llama*100,
+        mean_filter_baseline_50*100, mean_filter_baseline_50_llama*100,
+        mean_filter_baseline_100*100, mean_filter_baseline_100_llama*100,
+    ]
+})
+plt.figure(figsize=(8, 5))
+ax = sns.barplot(data=mean_df, x='History', y='Mean_WER', hue='Filtering based on LLM')
+ax.axhline(
+    y=mean_baseline * 100,
+    color='black',
+    linestyle='dashed',
+    linewidth=1.2,
+    label='Baseline non filtering'
+)
+# Add LLama have baseline as a line
+print(mean_filter_baseline_10)
+print(mean_filter_baseline_50)
+print(mean_filter_baseline_100)
+
+for container in ax.containers:
+    ax.bar_label(container, fmt='%.4f%%', padding=3)
+
+# Grid settings
+ax.yaxis.grid(True, linestyle='--', linewidth=0.7)
+
+# Grab all handles and labels (from both bars and the line)
+handles, labels = ax.get_legend_handles_labels()
+
+# Remove duplicates, just in case
+unique = dict(zip(labels, handles))
+
+# Set the updated legend
+ax.legend(unique.values(), unique.keys(), title='')
+ax.set_axisbelow(True)  # Ensure grid is behind the bars
+
+plt.title('')
+plt.ylabel('Mean WER(%)')
+plt.xlabel('History Length')
+plt.ylim(0, max(mean_df['Mean_WER'])+5)  # Optional: adjust Y-axis
+plt.tight_layout()
+plt.show()
---
+++ src/experiments/understanding_experiments/understanding_effect_dialect.py
@@ -0,0 +1,126 @@
+import json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+with open(f'../NPSC/NPSC_1/NPSC_2_0_test.jsonl', 'r') as file:
+    wer_data_points_df = pd.read_json(file, lines=True)
+
+dialect_files = wer_data_points_df.groupby('speaker_dialect')['audio'].apply(list).to_dict()
+
+norwegian_written_language_files = wer_data_points_df.groupby('sentence_language_code')['audio'].apply(list).to_dict()
+
+gender_filer = wer_data_points_df.groupby('speaker_gender')['audio'].apply(list).to_dict()
+
+def get_wer_lists(file_name, dialect_dict):
+    with open(f'../result/{file_name}', 'r') as file:
+        wer_data = json.load(file)
+
+    with open(f'../result/wer_npsc_experiment_27.json', 'r') as file:
+        wer_data_baseline = json.load(file)
+
+    # Create dict to collect WERs per dialect
+    wer_by_dialect = {dialect: [] for dialect in dialect_dict}
+    werr_by_dialect = {dialect: [] for dialect in dialect_dict}
+    print("Processing: ", file_name)
+
+    for idx, data in enumerate(wer_data):
+
+        audio_file = data.get("audio_file")
+        wer = data.get("wer_result")
+        wer_baseline = wer_data_baseline[idx].get("wer_result")
+        for dialect, audio_list in dialect_dict.items():
+            if audio_file in audio_list:
+                if(wer_baseline != 0):
+                    werr_by_dialect[dialect].append(((wer_baseline-wer)/wer_baseline)*100)
+                wer_by_dialect[dialect].append(wer)
+    return wer_by_dialect, werr_by_dialect
+
+def handle_processing(experiment_1, experiment_2, expeirment_3, dict_use, filtering_element_str, x_label):
+
+    wer_by_element_history_10, werr_by_element_history_10 = get_wer_lists(experiment_1, dict_use)
+    wer_by_element_history_50, werr_by_element_history_50 = get_wer_lists(experiment_2, dict_use)
+    wer_by_element_history_100, werr_by_element_history_100 = get_wer_lists(expeirment_3, dict_use)
+
+
+    combined_data_werr = []
+
+    for history, wer_dict in zip(
+        [10, 50, 100],
+        [werr_by_element_history_10, werr_by_element_history_50, werr_by_element_history_100]
+    ):
+        for filtering_element, werr_list in wer_dict.items():
+            for werr in werr_list:
+                combined_data_werr.append({
+                    filtering_element_str: filtering_element,
+                    "werr": werr,
+                    "history": history
+                })
+    
+    # Create DataFrame
+    df_werr = pd.DataFrame(combined_data_werr)
+    plt.figure(figsize=(10, 6))
+    df_werr["history"] = df_werr["history"].astype(str)
+    ax = sns.barplot(
+        data=df_werr,
+        x=filtering_element_str,
+        y='werr',
+        hue='history',
+        estimator='mean',
+        ci='sd'  # Standard deviation as error bars
+    )
+
+    for container in ax.containers:
+        ax.bar_label(container, fmt='%.2f', padding=3)
+    plt.xlabel(x_label)
+    plt.ylabel("Mean WERR")
+    plt.grid(True, axis='y')
+    plt.tight_layout()
+    plt.show()
+
+handle_processing("wer_npsc_experiment_12_llm.json","wer_npsc_experiment_13_llm.json","wer_npsc_experiment_14_llm.json", dialect_files, "dialect", "Speaker Dialect")
+# wer_by_dialect_history_10, werr_by_dialect_history_10 = get_wer_lists("wer_npsc_experiment_12_llm.json", dialect_files)
+# wer_by_dialect_history_50, werr_by_dialect_history_50 = get_wer_lists("wer_npsc_experiment_13_llm.json", dialect_files)
+# wer_by_dialect_history_100, werr_by_dialect_history_100 = get_wer_lists("wer_npsc_experiment_14_llm.json", dialect_files)
+# # Combine all into one list of dictionaries
+
+# combined_data_werr = []
+
+# for history, wer_dict in zip(
+#     [10, 50, 100],
+#     [werr_by_dialect_history_10, werr_by_dialect_history_50, werr_by_dialect_history_100]
+# ):
+#     for dialect, werr_list in wer_dict.items():
+#         for werr in werr_list:
+#             combined_data_werr.append({
+#                 "dialect": dialect,
+#                 "werr": werr,
+#                 "history": history
+#             })
+
+# # Create DataFrame
+# df_werr = pd.DataFrame(combined_data_werr)
+# plt.figure(figsize=(10, 6))
+# df_werr['history'] = df_werr['history'].astype(str)
+# ax = sns.barplot(
+#     data=df_werr,
+#     x='dialect',
+#     y='werr',
+#     hue='history',
+#     estimator='mean',
+#     ci='sd'  # Standard deviation as error bars
+# )
+
+# for container in ax.containers:
+#     ax.bar_label(container, fmt='%.2f', padding=3)
+
+# plt.title("Mean WERR per Dialect by History Length (with Std Dev)")
+# plt.xlabel("Speaker Dialect")
+# plt.ylabel("Mean WERR")
+# plt.grid(True, axis='y')
+# plt.tight_layout()
+# plt.show()
+
+handle_processing("wer_npsc_experiment_12_llm.json","wer_npsc_experiment_13_llm.json","wer_npsc_experiment_14_llm.json", norwegian_written_language_files, "language", "Transcription language")
+
+handle_processing("wer_npsc_experiment_12_llm.json","wer_npsc_experiment_13_llm.json","wer_npsc_experiment_14_llm.json", gender_filer, "gender", "Speaker gender")
