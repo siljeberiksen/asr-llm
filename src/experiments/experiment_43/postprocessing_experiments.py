1f63551378ab52665487c332887b6ea5deab0264
Silje Birkeland Eriksen <siljeberiksen@gmail.com>
2025-04-28 14:33:02 +0200

    37 postprocesing

--- src/experiments/experiment_43/postprocessing_experiments.py
+++ src/experiments/experiment_43/postprocessing_experiments.py
@@ -75,4 +75,4 @@ empty_instances = find_empty_instances("beam_npsc_experiment_43_llm.json")
 post_process("wer_npsc_experiment_43_llm.json", empty_instances)
 
 print("\nEmissions")
-runEmissionPostProcessing("experiment_43", "emissions_43.csv")
+runEmissionPostProcessing("experiment_37", "emissions_37.csv")
